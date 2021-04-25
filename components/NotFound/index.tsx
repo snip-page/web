a04fe09a209852c293b89890c9817998f14356f6
kenmueller <kenmueller0@gmail.com>
2021-04-24 20:24:45 -0700

    404 page

---
+++ components/NotFound/index.tsx
@@ -0,0 +1,24 @@
+import { NextPage } from 'next'
+
+import Head from 'components/Head'
+import Tabs from 'components/Tabs'
+import HomeTab from 'components/Tabs/Home'
+import SnipTab from 'components/Tabs/Snip'
+
+import styles from './index.module.scss'
+
+const NotFound: NextPage = () => (
+	<div className={styles.root}>
+		<Head
+			title="404.txt | snip"
+			description="Uh oh! There's nothing at this URL."
+		/>
+		<Tabs>
+			<HomeTab />
+			<SnipTab snip={null} />
+		</Tabs>
+		<p className={styles.message}>not found</p>
+	</div>
+)
+
+export default NotFound
