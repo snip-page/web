782e627cab0c57c4be53b06a29ec6122a62772b5
kenmueller <kenmueller0@gmail.com>
2021-04-24 21:52:24 -0700

    subtitle

--- components/Home/Info/index.tsx
+++ components/Home/Info/index.tsx
@@ -1,11 +1,11 @@
 import styles from './index.module.scss'
 
+export const SUBTITLE =
+	'Share your code with one command. Allow others to run your code in an online IDE. Embed runnable code on your website.'
+
 const HomeInfo = () => (
 	<section className={styles.root}>
-		<p className={styles.subtitle}>
-			Share your code with one command. Allow others to run your code in an
-			online IDE.
-		</p>
+		<p className={styles.subtitle}>{SUBTITLE}</p>
 	</section>
 )
 
--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -5,7 +5,7 @@ import RecentSnipsRequest from 'lib/snip/recent/request'
 import getRecentSnips from 'lib/snip/recent'
 import Head from 'components/Head'
 import Navbar from 'components/Navbar'
-import Info from './Info'
+import Info, { SUBTITLE } from './Info'
 import Install from './Install'
 import RecentSnips from 'components/Snip/Recent'
 
@@ -13,12 +13,7 @@ import styles from './index.module.scss'
 
 const Home: NextPage<HomeProps> = ({ snips }) => (
 	<div className={styles.root}>
-		<Head
-			title="snip - share your code"
-			description="Share your code with one command. Allow others to run your code in an
-			online IDE."
-			image=""
-		/>
+		<Head title="snip - share your code" description={SUBTITLE} image="" />
 		<Navbar />
 		<Info />
 		<Install />
