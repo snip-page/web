c3939bc8984f4d09cc796579fb2c8f333e18da72
kenmueller <kenmueller0@gmail.com>
2021-04-25 22:13:20 -0700

    puppeteer no sandbox

--- server/lib/snip/image/browser.ts
+++ server/lib/snip/image/browser.ts
@@ -2,6 +2,7 @@ import puppeteer, { Browser } from 'puppeteer'
 
 let browser: Browser | null = null
 
-const getBrowser = async () => (browser ??= await puppeteer.launch())
+const getBrowser = async () =>
+	(browser ??= await puppeteer.launch({ args: ['--no-sandbox'] }))
 
 export default getBrowser
