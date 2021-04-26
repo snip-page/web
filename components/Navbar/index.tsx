e4c392ec015f00e7da32dc8a2bdac2e1c0d19746
kenmueller <kenmueller0@gmail.com>
2021-04-26 01:42:30 -0700

    new link

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
+import { Svg } from 'react-optimized-image'
+
+import createIcon from 'images/plus.svg'
+
 import styles from './index.module.scss'
 
 const Navbar = () => (
 	<div className={styles.root}>
 		<nav className={styles.content}>
 			<h1 className={styles.title}>snip</h1>
+			<Link href="/new">
+				<a className={styles.create}>
+					<Svg className={styles.createIcon} src={createIcon} />
+					new
+				</a>
+			</Link>
 		</nav>
 	</div>
 )
